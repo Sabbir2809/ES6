9106952e53430672af4b3110824a43e2e0004915
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-07-19 21:26:38 +0600

    anonymous & return function

---
+++ ES6 Function/return_function.js
@@ -0,0 +1,15 @@
+function Hi() {
+  let msg = "Hi, I'm JavaScript";
+  return msg;
+}
+
+function Hello() {
+  return "Hello, I'm ES6";
+}
+console.log(Hello());
+
+// Function Can be return another function.
+function letsDoIt() {
+  return Hi();
+}
+console.log(Hi());
