88429bee3cd89feeb6dce4f7302dc2cd676237e7
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-07-21 12:57:56 +0600

    Map array

---
+++ ES6 Array/map_array.js
@@ -0,0 +1,14 @@
+// Map is a collection of Data.
+let visitedDivision = new Map();
+
+// keys & values pair.
+visitedDivision.set("key1", "Dhaka");
+visitedDivision.set("key2", "Sylhet");
+visitedDivision.set("key3", "Rajshahi");
+visitedDivision.set("key4", "Rongpur");
+visitedDivision.set("key5", "Chittagong");
+visitedDivision.set("key6", "Mymensingh");
+
+// print
+console.log(visitedDivision.values());
+console.log(visitedDivision.keys());
